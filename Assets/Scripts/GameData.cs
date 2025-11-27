5c34daf7294451ea790a5d1f8a62f561b58e2244
aj <65269484+aiden-ji@users.noreply.github.com>
2025-11-27 00:11:26 -0800

    refactor: Prune repository to only store build data

--- Assets/Scripts/GameData.cs
+++
@@ -1,7 +0,0 @@
-// This is a static class to hold data that needs to persist between scenes,
-// such as the currently selected song.
-public static class GameData
-{
-    public static SongInfo SelectedSongInfo { get; set; }
-    public static bool IsInitialCalibration { get; set; } = false;
-}
